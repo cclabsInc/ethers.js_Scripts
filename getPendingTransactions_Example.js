4383ea801b6e2322cb7bf649a64817538efe28d6
Ficti0n <33992409+cclabsInc@users.noreply.github.com>
2022-10-08 00:47:38 -0400

    Create getPendingTransactions_Example.js

---
+++ getPendingTransactions_Example.js
@@ -0,0 +1,22 @@
+require ('dotenv').config();
+const {ethers} =require("ethers")
+
+const provider = new ethers.providers.WebSocketProvider(process.env.INFURA_WS)
+
+const getPending = async () => {
+    provider.on("pending", async (txhash) => {
+
+        const transaction = await provider.getTransaction(txhash)
+
+        if (transaction != null) {
+            
+            gasprice = transaction.gasPrice
+            convertedPrice = ethers.utils.formatEther(gasprice)
+            console.log(`Confirmation: ${transaction.confirmations} \n Hash: ${transaction.hash} \n ${convertedPrice} \n ${gasprice}`)
+        }
+    })
+
+    
+}
+
+getPending()
